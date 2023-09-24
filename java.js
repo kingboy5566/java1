8a1610c7d6595e35d4b21d9fc91025cc996f7a25
muhammad45401 <saadjani3333@gmail.com>
2023-09-25 01:58:43 +0500

    java1

---
+++ java.js
@@ -0,0 +1,50 @@
+var userName ="muhammad "
+var myName = "ilyas"
+ var fullName = userName +myName
+console.log(fullName)
+
+
+var js ="Hello World"
+ alert(js)
+
+var fristName ="muhammad "
+var lastName ="ilyas"
+var fullName=fristName+lastName
+alert(fullName)
+
+var eage ="32 year old "
+alert(eage)
+
+var certifact ="i am studant of CCO 166966 "
+alert(certifact )
+
+var one="PIZZA\n"
+var two="PIZZ\n"
+var three="PIZ\n"
+var four="PI\n"
+var five="P\n"
+var pizza =(one+two+three+four+five)
+alert(pizza)
+
+
+var email =" my email adrees is saadjani33333@gmail.com"
+alert(email)
+
+var book ="i am tring to learn from tha Book a master\n way to learn javaScript"
+ alert(book)
+
+var style ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+ alert (style)
+
+
+
+
+
+
+
+
+
+
+
+
+
